7cc6e7ae87afc7ab2567aaf2f6b923935d12b4ce
danimohli <danimohlife@gmail.com>
2024-01-27 10:03:00 -0800

    readme

---
+++ REAMME.md
@@ -0,0 +1,2 @@
+One of and first alx peer projects of the one year programme in software engineering.
+Printf project is similar to C programming printf function.
---
+++ print.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * _printf - similar to printf
+ * @format: constant format
+ * Return: len of format
+ */
+int _printf(const char *format, ...)
+{
+	while (format != '\0')
+	{
+		if ()
+	}
+}
