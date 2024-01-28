14886103ae360fca3cc4aa5acc72fb47ff52f5c0
danimhli <danimhlife@gmail.com>
2024-01-29 00:17:19 +0100

    first

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	_printf("Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
+	return (0);
+}
--- print.c
+++ print.c
@@ -1,5 +1,4 @@
-#include <stdlib.h>
-#include <stdarg.h>
+#include "main.h"
 /**
  * _printf - similar to printf
  * @format: constant format
@@ -7,8 +6,36 @@
  */
 int _printf(const char *format, ...)
 {
-	while (format != '\0')
+	va_list args;
+	int x = 0;
+	char *str, ch;
+
+	va_start(args, format);
+	while (format[x] != '\0')
 	{
-		if ()
+
+		if (format[x] == '%')
+		{
+			if (format[x + 1] == 's')
+			{
+				str = va_arg(args, char *);
+				puts(str);
+			}
+			else if (format[x + 1] == 'c')
+			{
+				ch = va_arg(args, char);
+				putchar(ch);
+			}
+			else if (format[x + 1] == '%')
+			{
+				ch = va_arg(args, char);
+				putchar(ch);
+			}
+		}
+		else
+			putchar(format[x]);
+
 	}
+	va_end(args);
+	return (x + 1);
 }
