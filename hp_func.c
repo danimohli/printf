f3cb1c15fec87b5667b83cf3e101a0712a6122b2
danimhli <danimhlife@gmail.com>
2024-01-29 10:58:47 +0100

    second

--- hp_func.c
+++ hp_func.c
@@ -5,16 +5,18 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		num = -n;
 	}
-
-	if ((num / 10) > 0)
+	else
+	{
+		num = n;
+	}
+	if (num / 10 > 0)
 		print_number(num / 10);
-
 	_putchar((num % 10) + '0');
 }
--- print.c
+++ print.c
@@ -8,43 +8,54 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int x = 0;
-	char *str, ch;
+	char *str;
+	char ch;
 	long int num;
-
+	int count = 0;
+	
 	va_start(args, format);
+
 	while (format[x] != '\0')
 	{
-
 		if (format[x] == '%')
 		{
 			++x;
 			if (format[x] == 's')
 			{
 				str = va_arg(args, char *);
-				puts(str);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					_putchar(*str);
+					count++;
+					str++;
+				}
 			}
 			else if (format[x] == 'c')
 			{
-				ch = (char) va_arg(args, int);
-				putchar(ch);
+				ch = (char)va_arg(args, int);
+				_putchar(ch);
+				count++;
 			}
 			else if (format[x] == '%')
 			{
-				ch = (char)va_arg(args, int);
-				putchar(ch);
+				_putchar('%');
+				count++;
 			}
 			else if (format[x] == 'i' || format[x] == 'd')
 			{
 				num = va_arg(args, int);
 				print_number(num);
-
 			}
 		}
 		else
-			putchar(format[x]);
+		{
+			_putchar(format[x]);
+			count++;
+		}
 		x++;
-
 	}
 	va_end(args);
-	return (x + 1);
+	return count;
 }
