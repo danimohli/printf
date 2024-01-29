f193c484ade937acf869c1d605e0ae7ff28b395c
chinyereerondu <chinyereerondu21@gmail.com>
2024-01-29 04:44:03 -0800

    code modularize

--- hp_func.c
+++ hp_func.c
@@ -1,8 +1,20 @@
 #include "main.h"
 /**
+ * print_char - print character
+ * @ch: character
+ * @count: count
+ * Return: void
+ */
+void print_char(char ch, int *count)
+{
+	putchar(ch);
+	(*count)++;
+}
+/**
  * print_number - Print number.
  * @n: numsto print
  * @c: count
+ * Return: void
  */
 void print_number(int n, int *c)
 {
@@ -26,5 +38,22 @@ void print_number(int n, int *c)
 		(*c)++;
 		n %= div;
 		div /= 10;
+	}
+}
+/**
+ * print_str - take string from printf
+ * @str: pointer to string
+ * @count: count len of item
+ * Return: void
+ */
+void print_str(const char *str, int *count)
+{
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		putchar(*str);
+		(*count)++;
+		str++;
 	}
 }
